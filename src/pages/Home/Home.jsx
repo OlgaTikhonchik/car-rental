647aa654bf6a480dfae8872930e65db62af1c84d
OlgaTikhonchik <olga882011@gmail.com>
2023-09-13 17:30:31 +0300

    update Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,21 @@
+import { Images, Text, Title } from './Home.styled';
+
+import tesla from '../../images/tesla.jpg';
+
 const Home = () => {
   return (
     <>
-      <h1>
-        Welcome! <br /> Welcome! Unlock the Road to Adventure with Our Car
-        Rental Service!
-      </h1>
-      <img
-        src="https://tripmydream.cc/travelhub/travel/blocks/20/5156/block_205156.jpg"
-        alt="car"
-      />
+      <Title>
+        Welcome! <br />
+        Unlock the Road to Adventure with Our Car Rental Service!
+      </Title>
+      <Images src={tesla} alt="car" />
 
-      <h2>
+      <Text>
         The ultimate solution for hassle-free car rentals! With a wide range of
         vehicles to choose from, our user-friendly interface simplifies the
         process of renting a car.
-      </h2>
+      </Text>
     </>
   );
 };
---
+++ src/pages/Home/Home.styled.jsx
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled';
+
+export const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 700;
+  margin: 0;
+  padding: 20px;
+  color: #824113;
+  text-align: center;
+  transition: 250ms cubic-bezier(0.4, 0, 0.2, 1) 0s;
+  text-transform: uppercase;
+
+  :hover,
+  :focus {
+    color: #d38146;
+  }
+`;
+
+export const Text = styled.h2`
+  font-size: 40px;
+  font-weight: 700;
+  margin: 0;
+  padding: 20px;
+  color: #824113;
+  text-align: center;
+  transition: 250ms cubic-bezier(0.4, 0, 0.2, 1) 0s;
+
+  :hover,
+  :focus {
+    color: #d38146;
+  }
+`;
+
+export const Images = styled.img`
+  display: block;
+  width: 100%;
+  border-radius: 20px;
+`;
