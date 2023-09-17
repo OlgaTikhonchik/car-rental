9f301536a167069aa747ee63639a080c1d69e868
OlgaTikhonchik <114959595+OlgaTikhonchik@users.noreply.github.com>
2023-09-17 11:31:03 +0000

    Deploying to gh-pages from @ OlgaTikhonchik/car-rental@8ef4c3906d82818ad3ac9e633bcf0c9177b695ba 🚀

--- static/js/733.6f9650e5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[733],{733:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("h1",{children:"Favorites"})}}}]);
-//# sourceMappingURL=733.6f9650e5.chunk.js.map
