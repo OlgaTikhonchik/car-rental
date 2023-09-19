72915496fc4ac500df1b6abc2f88300c8aa20073
OlgaTikhonchik <olga882011@gmail.com>
2023-09-20 00:40:21 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,8 +10,8 @@ import {
   REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import { rootReducer } from './rootReducer';
-import { favoriteReducer, favoriteSlice } from './favoriteSlice';
+
+import { favoriteReducer } from './favoriteSlice';
 import { carsReducer } from './carsSlice';
 import { filterReducer } from './filterSlice';
 
@@ -25,9 +25,6 @@ const persistedReducer = persistReducer(persistConfig, favoriteReducer);
 
 export const store = configureStore({
   reducer: {
-    // cars: persistedReducer,
-
-    // favorite: favoriteSlice.reducer,
     cars: carsReducer,
     filter: filterReducer,
     favorite: persistedReducer,
