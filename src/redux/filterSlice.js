5e8acea7386c6a7dcacc908b545af30df43fc8b1
OlgaTikhonchik <olga882011@gmail.com>
2023-09-21 20:42:15 +0300

    Update filterSlice.js

--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -1,14 +1,32 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const filterInitialState = {
+  onFilter: false,
+  brand: '',
+  price: '',
+  mileageFrom: '',
+  mileageTo: '',
+};
+
 const filterSlice = createSlice({
   name: 'filter',
-  initialState: '',
+  initialState: filterInitialState,
   reducers: {
-    setFilter(_, action) {
-      return action.payload;
+    setFilter(state, { payload }) {
+      state.brand = payload.brand;
+      state.price = payload.price;
+      state.mileageFrom = payload.mileageFrom;
+      state.mileageTo = payload.mileageTo;
+      state.onFilter = payload.onFilter;
     },
   },
 });
 
 export const { setFilter } = filterSlice.actions;
 export const filterReducer = filterSlice.reducer;
+
+//  const selectOnFilter = state => state.filter.onFilter;
+//  const selectBrand = state => state.filter.brand;
+//  const selectPrice = state => state.filter.price;
+// const selectMileageFrom = state => state.filter.mileageFrom;
+// const selectMileageTo = state => state.filter.mileageTo;
