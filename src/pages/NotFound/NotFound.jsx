0dc4f025814acddb90e1491237aeac1baad40fcc
OlgaTikhonchik <olga882011@gmail.com>
2023-09-21 13:48:28 +0300

    update Not Found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -5,8 +5,12 @@ const NotFound = () => {
     <div>
       <TitleNotFound> Page not found </TitleNotFound>
       <TextNotFound>
-        Please, return to the home page. And try to enter a new movie!
+        Please, return to the home page. And try to enter a new car!
       </TextNotFound>
+      <img
+        src="https://png.pngtree.com/png-clipart/20230825/original/pngtree-404-error-people-failure-energy-picture-image_8729597.png"
+        alt="Not Found"
+      />
     </div>
   );
 };
--- src/pages/NotFound/NotFound.styled.jsx
+++ src/pages/NotFound/NotFound.styled.jsx
@@ -1,12 +1,13 @@
 import styled from '@emotion/styled';
 
 export const TitleNotFound = styled.h1`
-  color: red;
+  color: #824255;
   text-align: center;
+  margin-bottom: 10px;
 `;
 
 export const TextNotFound = styled.p`
-  font-size: 18px;
-  color: red;
+  font-size: 22px;
+  color: #824255;
   text-align: center;
 `;
