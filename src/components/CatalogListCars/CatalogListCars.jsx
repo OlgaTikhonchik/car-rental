d3425c3832cfc5dc3d5df29e1fe601b1c66175bc
OlgaTikhonchik <olga882011@gmail.com>
2023-11-07 15:57:00 +0200

    update Catalog page

--- src/components/CatalogListCars/CatalogListCars.jsx
+++ src/components/CatalogListCars/CatalogListCars.jsx
@@ -1,6 +1,71 @@
+// import CatalogItemCar from 'components/CatalogItemCar/CatalogItemCar';
+// import { useSelector } from 'react-redux';
+// import { CatalogItem, Container, List } from './CatalogListCar.styled';
+// import { selectCars, selectFiltredCars } from 'redux/selectors';
+
+// const CatalogListCars = () => {
+//   const cars = useSelector(selectCars);
+//   console.log(cars);
+//   const store = useSelector(store => store.cars);
+//   console.log(store);
+//   const filtred = useSelector(selectFiltredCars);
+//   const render = filtred ? filtred : cars;
+
+//   return (
+//     <Container>
+//       <List>
+//         {render.map(
+//           ({
+//             id,
+//             model,
+//             make,
+//             year,
+//             rentalPrice,
+//             address,
+//             rentalCompany,
+//             functionalities,
+//             type,
+//             img,
+//             fuelConsumption,
+//             engineSize,
+//             description,
+//             accessories,
+//             rentalConditions,
+//             mileage,
+//           }) => (
+//             <CatalogItem key={id}>
+//               <CatalogItemCar
+//                 key={id}
+//                 model={model}
+//                 make={make}
+//                 year={year}
+//                 rentalPrice={rentalPrice}
+//                 address={address}
+//                 rentalCompany={rentalCompany}
+//                 functionalities={functionalities}
+//                 id={id}
+//                 type={type}
+//                 img={img}
+//                 fuelConsumption={fuelConsumption}
+//                 engineSize={engineSize}
+//                 description={description}
+//                 accessories={accessories}
+//                 rentalConditions={rentalConditions}
+//                 mileage={mileage}
+//               />
+//             </CatalogItem>
+//           )
+//         )}
+//       </List>
+//     </Container>
+//   );
+// };
+
+// export default CatalogListCars;
+///
 import CatalogItemCar from 'components/CatalogItemCar/CatalogItemCar';
 import { useSelector } from 'react-redux';
-import { CatalogItem, Container, List } from './CatalogListCar.styled';
+import { Container, List } from './CatalogListCar.styled';
 import { selectCars, selectFiltredCars } from 'redux/selectors';
 
 const CatalogListCars = () => {
@@ -9,53 +74,15 @@ const CatalogListCars = () => {
   const store = useSelector(store => store.cars);
   console.log(store);
   const filtred = useSelector(selectFiltredCars);
+  console.log('filtred', filtred);
   const render = filtred ? filtred : cars;
 
   return (
     <Container>
       <List>
-        {render.map(
-          ({
-            id,
-            model,
-            make,
-            year,
-            rentalPrice,
-            address,
-            rentalCompany,
-            functionalities,
-            type,
-            img,
-            fuelConsumption,
-            engineSize,
-            description,
-            accessories,
-            rentalConditions,
-            mileage,
-          }) => (
-            <CatalogItem key={id}>
-              <CatalogItemCar
-                key={id}
-                model={model}
-                make={make}
-                year={year}
-                rentalPrice={rentalPrice}
-                address={address}
-                rentalCompany={rentalCompany}
-                functionalities={functionalities}
-                id={id}
-                type={type}
-                img={img}
-                fuelConsumption={fuelConsumption}
-                engineSize={engineSize}
-                description={description}
-                accessories={accessories}
-                rentalConditions={rentalConditions}
-                mileage={mileage}
-              />
-            </CatalogItem>
-          )
-        )}
+        {render.map(car => (
+          <CatalogItemCar key={car.id} dataCar={car} />
+        ))}
       </List>
     </Container>
   );
--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,5 +1,5 @@
 import CatalogListCars from 'components/CatalogListCars/CatalogListCars';
-import { getAllCars } from 'redux/operations';
+import { getAllCars, getAllCarsWithoutPage } from 'redux/operations';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -8,13 +8,21 @@ import { Button } from './Catalog.styled';
 import Filter from 'components/Filter/Filter';
 import { clearCarsData } from 'redux/carsSlice';
 import { Helmet } from 'react-helmet';
-import { selectError, selectIsLoading } from 'redux/selectors';
+import {
+  selectAllCars,
+  selectError,
+  selectIsLoading,
+  selectOnFilter,
+} from 'redux/selectors';
 
 const Catalog = () => {
   const dispatch = useDispatch();
   const [page, setPage] = useState(1);
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
+  const onFilter = useSelector(selectOnFilter);
+  const allCars = useSelector(selectAllCars);
+  console.log('allCars', allCars);
 
   const handleLoadClick = () => setPage(page => page + 1);
 
@@ -25,6 +33,12 @@ const Catalog = () => {
   useEffect(() => {
     dispatch(getAllCars(page));
   }, [dispatch, page]);
+
+  useEffect(() => {
+    if (onFilter) {
+      dispatch(getAllCarsWithoutPage());
+    }
+  }, [dispatch, onFilter]);
 
   return (
     <>
@@ -39,7 +53,7 @@ const Catalog = () => {
 
       {error && <b>Error: {error}</b>}
 
-      {36 / 8 > page && !isLoading && (
+      {allCars.length / 8 > page && !isLoading && onFilter && (
         <Button onClick={handleLoadClick}>Load more</Button>
       )}
     </>
